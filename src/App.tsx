08d2dceb4214ed2ef82009f937a642ec9fb0e11a
Fillippe <chiniara.fillippe@gmail.com>
2021-06-27 14:26:38 -0300

    adiciona rolagem de dados

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route, Link, useHistory, Redirect } from "react-router-dom";
 import { Button } from "react-bootstrap";
-
+import { faDice } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import routes from "./routes";
+import DiceRollerModal from "./components/dice-roller-modal/DiceRollerModal";
 
 const App = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   let history = useHistory();
 
   const handleBack = () => {
@@ -20,6 +27,9 @@ const App = () => {
           <h1 className="title">ESCUDEIRO</h1>
         </Link>
         <Button onClick={handleBack}>Voltar</Button>
+        <Button variant="warning" onClick={handleShow}>
+          <FontAwesomeIcon icon={faDice} />
+        </Button>
       </header>
       <Switch>
         <Route exact path="/">
@@ -29,6 +39,7 @@ const App = () => {
           return <RouteWithSubRoutes key={i} {...route} />;
         })}
       </Switch>
+      <DiceRollerModal isVisible={show} onCancel={handleClose} />
     </div>
   );
 };
---
+++ src/components/dice-roller-modal/DiceRollerModal.tsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  Form,
+  FormControl,
+  FormGroup,
+  ListGroup,
+  Modal,
+  Row,
+} from "react-bootstrap";
+import "./DiceRollerModal.scss";
+
+const DiceRollerModal = ({
+  onCancel,
+  isVisible,
+}: {
+  onCancel: () => void;
+  isVisible: boolean;
+}) => {
+  const [rollHistory, setRollHistory] = useState<string[]>([]);
+  const [modifier, setModifier] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    document.querySelector(".anchor")?.scrollIntoView();
+  });
+
+  const rollDice = (faces: number) => {
+    return Math.ceil(Math.random() * faces);
+  };
+
+  const renderDice = (diceList: Die[]) => {
+    return diceList.map((die, index) => {
+      return (
+        <Button
+          key={index}
+          onClick={() => {
+            setRollHistory([
+              ...rollHistory,
+              addRollToHistory(quantity, die.faces, modifier),
+            ]);
+          }}
+        >
+          D{die.faces}
+        </Button>
+      );
+    });
+  };
+
+  const parseDiceQuantity = (e: any) => {
+    setQuantity(
+      (parseInt(e.target.value!) ?? 1) < 1 ? 1 : parseInt(e.target.value!) ?? 1
+    );
+  };
+
+  const parseModifier = (e: any) => {
+    setModifier(parseInt(e.target.value!) ?? 0);
+  };
+
+  const addRollToHistory = (
+    quantity: number,
+    faces: number,
+    modifier: number
+  ) => {
+    const results = [];
+    for (let i = 0; i < quantity; i++) {
+      results.push(rollDice(faces));
+    }
+    return `[${results.join(",")}] ${quantity}d${faces} ${
+      modifier >= 0 ? "+" : "-"
+    } ${Math.abs(modifier)} = ${
+      results.reduce((total, result) => total + result, 0) + modifier
+    }`;
+  };
+
+  const diceList: Die[] = [
+    { faces: 4 },
+    { faces: 6 },
+    { faces: 8 },
+    { faces: 10 },
+    { faces: 12 },
+    { faces: 20 },
+    { faces: 100 },
+  ];
+
+  return (
+    <Modal
+      size="sm"
+      dialogClassName="dice-roller-modal"
+      show={isVisible}
+      onHide={onCancel}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Rolagem de Dados</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <ListGroup className="dice-roll-history">
+            {rollHistory.map((roll, index) => {
+              return <ListGroup.Item key={index}>{roll}</ListGroup.Item>;
+            })}
+            <div className="anchor"></div>
+          </ListGroup>
+        </Container>
+        <Container>
+          <Card className="dice-bar">
+            <Card.Body>
+              <Row>
+                <Col>
+                  <div className="quantity">
+                    <Form.Group controlId="quantity">
+                      <Form.Label>Quantidade</Form.Label>
+
+                      <Form.Control
+                        min="1"
+                        inputMode="numeric"
+                        type="number"
+                        value={quantity}
+                        onChange={(e) => parseDiceQuantity(e)}
+                      ></Form.Control>
+                    </Form.Group>
+                  </div>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col>
+                  <div className="dice">{renderDice(diceList)}</div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <div className="modifier">
+                    <Form.Group controlId="modifier">
+                      <Form.Label>Modificador</Form.Label>
+
+                      <FormControl
+                        type="number"
+                        value={modifier}
+                        onChange={(e) => parseModifier(e)}
+                      ></FormControl>
+                    </Form.Group>
+                  </div>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Container>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onCancel}>
+          Fechar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default DiceRollerModal;
+
+type Die = { faces: number };
